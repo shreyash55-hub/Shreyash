e9b196e1f339124e6a16ebe9ea993755b49b2304
Shreyash Sahu <Shreyash Sahu@DESKTOP-5EC5EI0>
2021-10-18 22:15:55 +0530

    commit all files

---
+++ Ecommerce/src/Mansi/Mansi.java
@@ -0,0 +1,15 @@
+package Mansi;
+
+public class Mansi {
+
+	public static void main(String args[]) {
+		Mansi m=new Mansi();
+		m.display();
+
+	}
+
+	public void display() {
+		System.out.println("Hello World");
+	}
+
+}
